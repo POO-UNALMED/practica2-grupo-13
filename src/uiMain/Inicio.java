ea94c4aace04da4b7b6182484e28fe87c73f42bd
sacastrot <70394887+sacastrot@users.noreply.github.com>
2020-11-19 01:42:59 -0500

    Inicio.java
    Eventos sobre imagenes de cultivos

--- src/uiMain/Inicio.java
+++ src/uiMain/Inicio.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
@@ -15,19 +16,34 @@ public class Inicio extends Application{
 
 	Label bienvenida = new Label("Welcome");
 	Label vida = new Label("HOjass de vida");
+	BorderPane p4 = new BorderPane();
+	Image cultivos1;
+	Image cultivos2;
+	Image cultivos3;
+	Image cultivos4;
+	Image cultivos5;
+	ImageView icultivos1;
+	ImageView icultivos2;
+	ImageView icultivos3;
+	ImageView icultivos4;
+	ImageView icultivos5;
 	
 	
 	public void start(Stage primaryStage) {
 		
-		VBox root0 = new VBox(5);
-		HBox root = new HBox(30);
+		//Nodo raiz de la escena
+		VBox root = new VBox(5);
+		
+		//Contiene todos los elementos de la venta Inicio
+		HBox contenedor = new HBox(30);
 		BorderPane p1 = new BorderPane();
 		BorderPane p2 = new BorderPane();
 		FlowPane p3 = new FlowPane();
-		BorderPane p4 = new BorderPane();
 		FlowPane p5 = new FlowPane();
 		GridPane p6 = new GridPane();
-		Button sigVentana = new Button("Pasar ventana");	
+		Button sigVentana = new Button("Pasar ventana");
+		
+		//Menu
 		MenuBar menuPrincipal = new MenuBar();
 		Menu barraPrincipal = new Menu("Inicio");
 		MenuItem salir = new MenuItem("Salir");
@@ -36,31 +52,57 @@ public class Inicio extends Application{
 		menuPrincipal.getMenus().addAll(barraPrincipal);
 		barraPrincipal.getItems().addAll(salir,separador,descripcion);
 		
-		root0.getChildren().add(menuPrincipal);
-		root0.getChildren().add(root);
+		root.getChildren().add(menuPrincipal);
+		root.getChildren().add(contenedor);
 		
-		Image cultivos1 = new Image(getClass().getResourceAsStream("./imagenes/cultivos1.jpg"));
-		ImageView icultivos1 = new ImageView(cultivos1);
-		ImageView icultivos2 = new ImageView(cultivos1);
-		ImageView icultivos3 = new ImageView(cultivos1);
-		ImageView icultivos4 = new ImageView(cultivos1);
-		ImageView icultivos5 = new ImageView(cultivos1);
+		// Imagenes de la app P4
+		cultivos1 = new Image(getClass().getResourceAsStream("./imagenes/cultivos1.jpg"));
+		cultivos2 = new Image(getClass().getResourceAsStream("./imagenes/cultivos2.jpg"));
+		cultivos3 = new Image(getClass().getResourceAsStream("./imagenes/cultivos3.jpg"));
+		cultivos4 = new Image(getClass().getResourceAsStream("./imagenes/cultivos4.jpg"));
+		cultivos5 = new Image(getClass().getResourceAsStream("./imagenes/cultivos5.jpg"));
+		icultivos1 = new ImageView(cultivos1);
+		icultivos2 = new ImageView(cultivos2);
+		icultivos3 = new ImageView(cultivos3);
+		icultivos4 = new ImageView(cultivos4);
+		icultivos5 = new ImageView(cultivos5);
 		
+		icultivos1.setOnMouseExited(mouseHandler);
+		icultivos2.setOnMouseExited(mouseHandler);
+		icultivos3.setOnMouseExited(mouseHandler);
+		icultivos4.setOnMouseExited(mouseHandler);
+		icultivos5.setOnMouseExited(mouseHandler);
 		icultivos1.setFitHeight(200);
 		icultivos1.setPreserveRatio(true);
-		icultivos2.setFitHeight(100);
+		icultivos2.setFitHeight(200);
 		icultivos2.setPreserveRatio(true);
-		icultivos3.setFitHeight(100);
+		icultivos3.setFitHeight(200);
 		icultivos3.setPreserveRatio(true);
-		icultivos4.setFitHeight(100);
+		icultivos4.setFitHeight(200);
 		icultivos4.setPreserveRatio(true);
-		icultivos5.setFitHeight(100);
+		icultivos5.setFitHeight(200);
 		icultivos5.setPreserveRatio(true);
+		
+		
+		ImageView fotoHojaVida1 = new ImageView(cultivos1);
+		ImageView fotoHojaVida2 = new ImageView(cultivos1);
+		ImageView fotoHojaVida3 = new ImageView(cultivos1);
+		ImageView fotoHojaVida4 = new ImageView(cultivos1);
+		
+	
+		fotoHojaVida1.setFitHeight(100);
+		fotoHojaVida1.setPreserveRatio(true);
+		fotoHojaVida2.setFitHeight(100);
+		fotoHojaVida2.setPreserveRatio(true);
+		fotoHojaVida3.setFitHeight(100);
+		fotoHojaVida3.setPreserveRatio(true);
+		fotoHojaVida4.setFitHeight(100);
+		fotoHojaVida4.setPreserveRatio(true);
 		
 		p1.setPrefSize(500,400);
 		p2.setPrefSize(500,400);
 		
-		root.getChildren().addAll(p1,p2);
+		contenedor.getChildren().addAll(p1,p2);
 		p1.setTop(p3);
 		p1.setCenter(p4);
 		p2.setTop(p5);
@@ -80,25 +122,51 @@ public class Inicio extends Application{
 		p6.setVgap(12);
 		p6.setHgap(12);
 		
-		p6.add(icultivos2, 0, 0);
-		p6.add(icultivos3, 0, 1);
-		p6.add(icultivos4, 1, 0);
-		p6.add(icultivos5, 1, 1);
+		p6.add(fotoHojaVida1, 0, 0);
+		p6.add(fotoHojaVida2, 0, 1);
+		p6.add(fotoHojaVida3, 1, 0);
+		p6.add(fotoHojaVida4, 1, 1);
 		p6.setAlignment(Pos.CENTER);
 		
 		primaryStage.setTitle("Inicio");
-		Scene scene = new Scene(root0, 860,350);
+		Scene scene = new Scene(root, 860,350);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
-	}
+	}  EventHandler<MouseEvent> mouseHandler = new EventHandler<MouseEvent>() {
+		int cont = 2;
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+        	String direction = "";
+        	if (cont==6) {
+				cont = 1;
+			}
+        	System.out.println(mouseEvent.getEventType());
+        		
+        	if (cont==1) {
+        		p4.setTop(null);
+            	p4.setTop(icultivos1);
+			}else if (cont==2) {
+				p4.setTop(null);
+            	p4.setTop(icultivos2);
+			}else if (cont==3) {
+				p4.setTop(null);
+            	p4.setTop(icultivos3);
+			}else if (cont==4) {
+				p4.setTop(null);
+            	p4.setTop(icultivos4);
+			}else if (cont==5) {
+				p4.setTop(null);
+            	p4.setTop(icultivos5);
+			}
+        	System.out.println(cont);
+        	cont++;
+        }
+    };
 	
 	
 	public static void main(String[] args) {
 		launch (args);
 	}
-	
-	
-	
-	
+
 }
