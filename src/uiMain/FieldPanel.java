6464a67442094fbc1ba68de2b9848aeaf09d4a5a
sacastrot <70394887+sacastrot@users.noreply.github.com>
2020-11-20 10:29:31 -0500

    FieldPane
    Creación de la clase

---
+++ src/uiMain/FieldPanel.java
@@ -0,0 +1,9 @@
+package uiMain;
+
+import javafx.scene.layout.Pane;
+
+public class FieldPanel extends Pane{
+	FieldPanel (String tituloCriterios, String[] criterios, String tituloValores, String[] valores, boolean[] habilitado) {
+		
+	}
+}
